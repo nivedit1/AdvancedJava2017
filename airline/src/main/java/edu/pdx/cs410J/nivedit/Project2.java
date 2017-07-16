79dbf5ddb174df1dfa08b6635dd820b773dd493f
Niveditha Venugopal <nivedit@pdx.edu>
2017-07-15 20:21:00 -0700

    Changed the validation into a method.

--- airline/src/main/java/edu/pdx/cs410J/nivedit/Project2.java
+++ airline/src/main/java/edu/pdx/cs410J/nivedit/Project2.java
@@ -23,19 +23,17 @@ public class Project2 {
         //Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
         Boolean readmeFlag = false;
         Boolean printFlag = false;
+        Boolean textFileFlag = false;
         String[] argumentArray = new String[100];
+        boolean validArguments = false;
         String airlineName = "";
         int flightNumber = 0;
         String sourceAirport = "";
-        Date departureTime = new Date();
-        String userInputDepartureTime = "";
+        String departureTime = "";
         String destinationAirport = "";
-        Date arrivalTime = new Date();
-        String userInputArrivalTime = "";
-        String expectedDatePattern = "MM/dd/yyyy HH:mm";
-        SimpleDateFormat formatter = new SimpleDateFormat(expectedDatePattern);
-        formatter.setLenient(false);
+        String arrivalTime = "";
         Collection<Flight> flights = new ArrayList<>();
+
         if (args.length == 1) {
             if (args[0].equals("-README")) {
                 readmeFlag = true;
@@ -45,92 +43,56 @@ public class Project2 {
                 printFlag = true;
                 System.err.println("Not enough arguments to print");
                 System.exit(1);
-            } else if (args[0].equals("-textFile")){
+            } else if (args[0].equals("-textFile")) {
                 System.err.println("No arguments to write to file");
-            }
-            else {
-                System.err.println("Missing command line arguments");
-                System.exit(1);
-              }
-        }
-        else if (args.length < 8) {
-            if (args[0].equals("-README") || args[1].equals("-README")) {
-                readmeFlag = true;
-                System.out.println(getReadme());
-                System.exit(0);
-            } else if ((args[0].equals("-print")) && readmeFlag == false) {
-                printFlag = true;
-                System.err.println("Not enough arguments to print!");
-                System.exit(1);
             } else {
                 System.err.println("Missing command line arguments");
                 System.exit(1);
             }
-        }
-        else if(args.length >= 8 && args.length <= 10){
-            if (args[0].equals("-README") || args[1].equals("-README")) {
+        } else if (args.length < 8) {
+            if (args[0].equals("-README") || args[1].equals("-README") || args[2].equals("-README")) {
                 readmeFlag = true;
                 System.out.println(getReadme());
-            } else if (args[0].equals("-print") && args.length == 9) {
+                System.exit(0);
+            } else if ((args[0].equals("-print") || args[1].equals("-print") || args[2].equals("-print"))) {
+                printFlag = true;
+                System.err.println("Not enough arguments to print!");
+                System.exit(1);
+            } else if ((args[0].equals("-textFile") || args[1].equals("-textFile") || args[2].equals("-textFile"))) {
+                System.err.println("Not enough arguments to write to file");
+            } else {
+                System.err.println("Missing command line arguments");
+                System.exit(1);
+            }
+        } else if (args.length >= 8 && args.length <= 10) {
+            if (args[0].equals("-README") || args[1].equals("-README") || args[2].equals("-README")) {
+                readmeFlag = true;
+                System.out.println(getReadme());
+            } else if ((args[0].equals("-print") || args[1].equals("-print")) && args.length == 9) {
                 printFlag = true;
                 for (int i = 1; i < args.length; i++) {
                     argumentArray[i - 1] = args[i];
                 }
-            } else if (args.length > 8){
+            } else if (args.length > 8) {
                 System.err.println("Too many arguments!");
                 System.exit(1);
             } else {
-                  for (int i = 0; i < args.length; i++) {
+                for (int i = 0; i < args.length; i++) {
                     argumentArray[i] = args[i];
-                  }
+                }
             }
-            if (readmeFlag == false) {
+            validArguments = validateArguments(argumentArray);
+            //System.out.println(validArguments);
+            if (validArguments == true){
                 airlineName = argumentArray[0];
-                try {
-                    flightNumber = Integer.parseInt(argumentArray[1]);
-                    if(flightNumber < 0){
-                        System.err.println("Flight number cannot be negative " + flightNumber);
-                        System.exit(1);
-                    }
-                } catch (Exception e) {
-                    System.err.println("Flight Number is not Integer " + argumentArray[1]);
-                    System.exit(1);
-                }
-                if(argumentArray[2].matches("^[a-zA-Z][a-zA-Z][a-zA-Z]") == true) {
-                  sourceAirport = argumentArray[2];
-                }
-                else {
-                    System.err.println("Source Airport Code is not a 3 letter code - " + argumentArray[2]);
-                    System.out.println("Airport code should be of the format \"AAA\"");
-                    System.exit(1);
-                }
-                try {
-                    userInputDepartureTime = argumentArray[3] + " " + argumentArray[4];
-                    departureTime = formatter.parse(userInputDepartureTime);
-                }
-                catch(Exception e) {
-                    System.err.println("The Departure time does not match the format \"MM/DD/YY HH:MM\" - " + userInputDepartureTime);
-                    System.exit(1);
-                }
-                if(argumentArray[5].matches("^[a-zA-Z][a-zA-Z][a-zA-Z]") == true) {
-                    destinationAirport = argumentArray[5];
-                }
-                else {
-                    System.err.println("Destination Airport Code is not a 3 letter code - " + argumentArray[5]);
-                    System.out.println("Airport code should be of the format \"AAA\"");
-                    System.exit(1);
-                }
-                try {
-                    userInputArrivalTime = argumentArray[6] + " " + argumentArray[7];
-                    arrivalTime = formatter.parse(userInputArrivalTime);
-                }
-                catch(Exception e) {
-                    System.err.println("The Arrival time does not match the format \"MM/DD/YY HH:MM\" - " + userInputArrivalTime);
-                    System.exit(1);
-                }
-                Flight flight = new Flight(flightNumber, sourceAirport, userInputDepartureTime, destinationAirport, userInputArrivalTime);
+                flightNumber = Integer.parseInt(argumentArray[1]);
+                sourceAirport = argumentArray[2];
+                departureTime = argumentArray[3] + " " + argumentArray[4];
+                destinationAirport = argumentArray[5];
+                arrivalTime = argumentArray[6] + " " + argumentArray[7];
+                Flight flight = new Flight(flightNumber, sourceAirport, departureTime, destinationAirport, arrivalTime);
                 flights.add(flight);
-                Airline airline = new Airline(airlineName,flights);
+                Airline airline = new Airline(airlineName, flights);
                 airline.addFlight(flight);
                 TextDumper t = new TextDumper("C:\\Users\\anant\\OneDrive\\Documents\\Niveditha" +
                         "\\PSU\\Summer 2017\\Advanced Java\\Git\\AdvancedJava2017\\airline\\src\\main\\java\\edu\\pdx\\cs410J\\nivedit\\dummy.txt");
@@ -149,53 +111,121 @@ public class Project2 {
                 readmeFlag = true;
                 System.out.println(getReadme());
                 System.exit(0);
-            } else if ((args[0].equals("-print")&& readmeFlag == false)) {
+            } else if ((args[0].equals("-print") && readmeFlag == false)) {
                 printFlag = true;
                 System.err.println("Too many arguments to print!");
                 System.exit(1);
-            } else {
+            } else if ((args[0].equals("-textFile")) || args[1].equals("-textFile") || args[2].equals("-textFile")){
+                textFileFlag = true;
+                System.err.println("Too many arguments to write to file");
+                System.exit(1);
+            }
+            else {
                 System.err.println("Too many arguments!");
                 System.exit(1);
             }
         }
+
     }
 
     /*This method returns a README for the application
+    *@return
     * */
     private static String getReadme() {
+
         String title = "\nName of the project:\n" +
-                "====================\n"+
+                "====================\n" +
                 "CS410J Project 1: Designing an Airline Application";
         String applicationDescription = "\n\nApplication Description:\n" +
-                            "========================\n"+
-                "This application parses the command line arguments provided by the user "+
-        "and assigns the values to the airline and the respective flight stated by the user.\n"+
-                "It also checks the validity of flight number, airport codes, and "+
+                "========================\n" +
+                "This application parses the command line arguments provided by the user " +
+                "and assigns the values to the airline and the respective flight stated by the user.\n" +
+                "It also checks the validity of flight number, airport codes, and " +
                 "arrival and departure times.\n";
-        String argumentList = "\nArguments List:"+
-                      "\n===============\n"+
-                "Pass arguments to this program as follows:\n"+
+        String argumentList = "\nArguments List:" +
+                "\n===============\n" +
+                "Pass arguments to this program as follows:\n" +
                 "  name -> The name of the airline  [Format: String]\n" +
                 "  flightNumber -> The Flight Number which identifies the flight  [Format: Numeric Code]\n" +
                 "  src -> Code of Departure airport  [Format: \"AAA\" (3 Letter Code)]\n" +
                 "  departTime -> Departure date and time (24-hour time)  [Format: mm/dd/yyyy hh:mm]\n" +
                 "  dest -> Code of Arrival airport  [Format: \"AAA\" (3 Letter Code)]\n" +
                 "  arriveTime -> Arrival date and time (24-hour time)  [Format: mm/dd/yyyy hh:mm]\n\n";
-        String options = "Options:"+
-                "\n========\n"+
+        String options = "Options:" +
+                "\n========\n" +
                 "Options are as follows:\n" +
                 "  -print -> Prints a description of the new flight\n" +
                 "  -README -> Prints a README for this project and exits\n";
-        String executionInstruction= "\nExecution Instruction:"+
-                      "\n======================\n"+
+        String executionInstruction = "\nExecution Instruction:" +
+                "\n======================\n" +
                 "Run the program with -> java edu.pdx.cs410J.nivedit.Project2 [options] <args>";
         String developedBy = "\n\nDeveloped By:\n" +
-                      "============="+
-                      "\n-Niveditha Venugopal"+
-                      "\n-Email ID: nivedit@pdx.edu"+
-                      "\n-PSU ID:   978073102";
+                "=============" +
+                "\n-Niveditha Venugopal" +
+                "\n-Email ID: nivedit@pdx.edu" +
+                "\n-PSU ID:   978073102";
         String readMeText = title + applicationDescription + argumentList + options + executionInstruction + developedBy;
         return readMeText;
+    }
+
+    /**
+     * This function validates the argument array
+     * @param argumentArray
+     * @return true if all the arguments have passed the test
+     */
+
+    private static boolean validateArguments(String[] argumentArray) {
+        String airlineName = "";
+        int flightNumber = 0;
+        String sourceAirport = "";
+        Date departureTime = new Date();
+        String userInputDepartureTime = "";
+        String destinationAirport = "";
+        Date arrivalTime = new Date();
+        String userInputArrivalTime = "";
+        String expectedDatePattern = "MM/dd/yyyy HH:mm";
+        SimpleDateFormat formatter = new SimpleDateFormat(expectedDatePattern);
+        formatter.setLenient(false);
+        airlineName = argumentArray[0];
+        try {
+            flightNumber = Integer.parseInt(argumentArray[1]);
+            if (flightNumber < 0) {
+                System.err.println("Flight number cannot be negative " + flightNumber);
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            System.err.println("Flight Number is not Integer " + argumentArray[1]);
+            System.exit(1);
+        }
+        if (argumentArray[2].matches("^[a-zA-Z][a-zA-Z][a-zA-Z]") == true) {
+            sourceAirport = argumentArray[2];
+        } else {
+            System.err.println("Source Airport Code is not a 3 letter code - " + argumentArray[2]);
+            System.out.println("Airport code should be of the format \"AAA\"");
+            System.exit(1);
+        }
+        try {
+            userInputDepartureTime = argumentArray[3] + " " + argumentArray[4];
+            departureTime = formatter.parse(userInputDepartureTime);
+        } catch (Exception e) {
+            System.err.println("The Departure time does not match the format \"MM/DD/YY HH:MM\" - " + userInputDepartureTime);
+            System.exit(1);
+        }
+        if (argumentArray[5].matches("^[a-zA-Z][a-zA-Z][a-zA-Z]") == true) {
+            destinationAirport = argumentArray[5];
+        } else {
+            System.err.println("Destination Airport Code is not a 3 letter code - " + argumentArray[5]);
+            System.out.println("Airport code should be of the format \"AAA\"");
+            System.exit(1);
+        }
+        try {
+            userInputArrivalTime = argumentArray[6] + " " + argumentArray[7];
+            arrivalTime = formatter.parse(userInputArrivalTime);
+        } catch (Exception e) {
+            System.err.println("The Arrival time does not match the format \"MM/DD/YY HH:MM\" - " + userInputArrivalTime);
+            System.exit(1);
+        }
+        return true;
     }
 }
 

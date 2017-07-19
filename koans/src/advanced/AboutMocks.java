9a2fcc44cd1bda9b8158aca03162b7c196349a97
Niveditha Venugopal <nivedit@pdx.edu>
2017-07-19 10:39:18 -0700

    Completed AboutMocks.java

--- koans/src/advanced/AboutMocks.java
+++ koans/src/advanced/AboutMocks.java
@@ -34,13 +34,19 @@ public class AboutMocks {
             return true;
         }
     }
+        static class SafeCollaborator implements Collaborator {
+            public  void doBusinessStuff(){
+
+            }
+        }
 
     @Koan
     public void simpleAnonymousMock() {
         // HINT: pass a safe Collaborator implementation to constructor
         // new ClassUnderTest(new Collaborator(){... it should not be the
         // objective of this test to test that collaborator, so replace it
-        new ClassUnderTest().doSomething();
+        Collaborator safeCollaborator = new SafeCollaborator();
+        new ClassUnderTest(safeCollaborator).doSomething();
     }
 
 }
